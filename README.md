e47394c777a312923d279ebad5077635b8f5ce1d
Paulo Gomes <paulo@appnovation.com>
2020-04-11 17:28:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# Contentful Better Slugs
+
+The Better Slugs is a simple UI extension for Contentful CMS that provides a more enhanced way to deal with slug fields.
+The existing slug functionality in Contentful is limited to one field (title) and therefore a bit limited for most of cases like:
+
+- Work with Gatsby Preview
+- Ability to automatically generate the slug with information from other fields (including referenced fields)
+- Autogeneration of slug doesn't end with entry is published
+
+## Overview
+
+The extension has the following features:
+
+- Generate a dynamic slug based on a pattern defined for each Content Model
+
+![Screencast](https://monosnap.com/image/5X63iQHS9YQ3AfYNPzEPZEIxAmTISY)
+
+## Requirements
+
+In order to use this extension you need:
+
+- the UI Extension has to be 3rd party hosted using the src property
+
+## Instalation (UI - using this repo)
+
+The UI Extension can be installed manually from the Contentful UI following the below steps:
+
+1. Navigate to Settings > Extensions
+2. Click on "Add extension > Install from Github"
+3. Use `https://raw.githubusercontent.com/pauloamgomes/contentful-better-slugs/master/extension.json` in the url
+4. On the extension settings screen change Hosting to Self-hosted using the url `https://pauloamgomes.github.io/contentful-better-slugs/`
+
+## Usage
+
+1. Add a new text field to your content model, it can be localized.
+2. On the Appearance tab ensure that Better Slugs is selected
+3. Provide your slug pattern, the pattern can use the following tokens:
+
+- **`[locale]`** - Will replace the token with the node locale
+- **`[field:your-field-name]`** - Will replace the token with the value of the field
+- **`[field:your-reference-field-name:field-name]`** - Will replace the token with the value of field that belongs to the reference.
+
+Example patterns:
+
+```
+[field:title]
+```
+
+Thats the default pattern, and probably for most of the cases would be enough.
+
+```
+[locale]/[field:category:title]/[field:title]
+```
+
+Assuming your locale is English, you have a reference field named category with title value `Computers` and your entry title is `Laptop 15"` the slug will be: `en/computers/laptop-15`
+
+```
+[field:date]/[field:title]
+```
+
+Assuming your locale is English, you have a date field named date with value `Friday, April 10th 2020` and your entry title is `London Event` the slug will be: `2020-04-10/london-event`
+
+## Optional Usage for Development
+
+After cloning, install the dependencies
+
+```bash
+yarn install
+```
+
+To bundle the extension
+
+```bash
+yarn build
+```
+
+To host the extension for development on `http://localhost:1234`
+
+```bash
+yarn start
+```
+
+To install the extension:
+
+```bash
+contentful extension update --force
+```
+
+## Limitations
+
+Tested only with text and date fields, so not sure how it can behave with custom fields, it will depend on the value stored in the field.
+
+## Todo
+
+- Improve the handling of reference fields
+- Improve the handling of date fields by providing a date format.
+
+## Copyright and license
+
+Copyright 2020 pauloamgomes under the MIT license.
---
+++ src/index.tsx
@@ -0,0 +1,168 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { init, FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
+//
+import './index.css';
+
+const uslug = require('uslug');
+
+interface BetterSlugsProps {
+  sdk: FieldExtensionSDK;
+}
+
+const BetterSlugs = ({ sdk }: BetterSlugsProps) => {
+  let debounceInterval: any = false;
+  let detachExternalChangeHandler: Function | null = null;
+
+  const pattern: string = sdk.parameters.instance.pattern;
+  const parts = pattern.split('/').map((part: string) => part.replace(/(\[|\])/gi, '').trim());
+
+  const [value, setValue] = React.useState('');
+  const fields: string[] = [];
+
+  React.useEffect(() => {
+    sdk.window.startAutoResizer();
+
+    // Extract fields used in slug parts.
+    parts.forEach((part: string) => {
+      if (part.startsWith('field:')) {
+        fields.push(part.replace('field:', ''));
+      }
+    });
+
+    // Create a listener for each field and matching locales.
+    fields.forEach((field: string) => {
+      const fieldParts = field.split(':');
+      const fieldName = fieldParts.length === 1 ? field : fieldParts[0];
+      if (Object.prototype.hasOwnProperty.call(sdk.entry.fields, fieldName)) {
+        const locales = sdk.entry.fields[fieldName].locales;
+
+        locales.forEach((locale: any) => {
+          sdk.entry.fields[fieldName].onValueChanged(locale, (value: any) => {
+            if (debounceInterval) {
+              clearInterval(debounceInterval);
+            }
+            debounceInterval = setTimeout(() => {
+              updateSlug(locale);
+            }, 500);
+          });
+        });
+      }
+    });
+
+    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+    detachExternalChangeHandler = sdk.field.onValueChanged(onExternalChange);
+
+    return () => {
+      if (detachExternalChangeHandler) {
+        detachExternalChangeHandler();
+      }
+    };
+  }, []);
+
+  /**
+   * Retrieves the raw value from a referenced field.
+   */
+  const getReferenceFieldValue = async (
+    fieldName: string,
+    subFieldName: string,
+    locale: string
+  ) => {
+    const defaultLocale = sdk.locales.default;
+    const referenceLocale = sdk.entry.fields[fieldName].locales.includes(locale)
+      ? locale
+      : defaultLocale;
+
+    const reference = sdk.entry.fields[fieldName].getValue(referenceLocale);
+    const result: any = await sdk.space.getEntry(reference.sys.id);
+    const { fields } = result;
+
+    if (!fields) {
+      return '';
+    }
+
+    if (!Object.prototype.hasOwnProperty.call(fields, subFieldName)) {
+      return '';
+    }
+
+    if (Object.prototype.hasOwnProperty.call(fields[subFieldName], locale)) {
+      return fields[subFieldName][locale];
+    }
+
+    if (Object.prototype.hasOwnProperty.call(fields[subFieldName], defaultLocale)) {
+      return fields[subFieldName][defaultLocale];
+    }
+
+    return '';
+  };
+
+  /**
+   * Updates the slug based on the defined pattern.
+   */
+  const updateSlug = async (locale: string) => {
+    const defaultLocale = sdk.locales.default;
+    const slugParts: string[] = [];
+
+    for (const part of parts) {
+      if (part.startsWith('field:')) {
+        const fieldParts = part.split(':');
+        let raw = '';
+
+        if (fieldParts.length === 2) {
+          if (sdk.entry.fields[fieldParts[1]] !== undefined) {
+            if (sdk.entry.fields[fieldParts[1]].locales.includes(locale)) {
+              raw = sdk.entry.fields[fieldParts[1]].getValue(locale);
+            } else {
+              raw = sdk.entry.fields[fieldParts[1]].getValue(defaultLocale);
+            }
+          }
+        } else {
+          raw = await getReferenceFieldValue(fieldParts[1], fieldParts[2], locale);
+        }
+
+        const slug = uslug(raw).replace(/[-_\ufe0f]+$/gu, '');
+
+        slugParts.push(slug);
+      } else if (part === 'locale') {
+        slugParts.push(locale);
+      } else {
+        slugParts.push(part);
+      }
+    }
+
+    sdk.entry.fields.slug.setValue(
+      slugParts
+        .join('/')
+        .replace('//', '/')
+        .replace(/\/$/, ''),
+      locale
+    );
+  };
+
+  const onExternalChange = (value: string) => {
+    setValue(value);
+  };
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+
+    setValue(value);
+
+    if (value) {
+      await sdk.field.setValue(value);
+    } else {
+      await sdk.field.removeValue();
+    }
+  };
+
+  return (
+    <div className="container">
+      <input width="large" id="slug-field" name="slug" value={value || ''} onChange={onChange} />
+      <button onClick={() => updateSlug(sdk.field.locale)}>reset</button>
+    </div>
+  );
+};
+
+init(sdk => {
+  render(<BetterSlugs sdk={sdk as FieldExtensionSDK} />, document.getElementById('root'));
+});
